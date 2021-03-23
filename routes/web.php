0b8620e945ed2974b561071b04039c79f831f3c9
valefalas <valefalas@gmail.com>
2021-03-23 16:08:42 -0300

    Mensaje del commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/welcome/{name}', [WelcomeController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
