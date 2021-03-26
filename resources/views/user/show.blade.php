44d1d4bb50eac164e640557c452cb1c273edae93
valefalas <valefalas@gmail.com>
2021-03-26 10:10:16 -0300

    Usuarios

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	
+</head>
+<body>
+	<div>
+		{{ $user->name }}
+	</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 Route::get('/welcome/{name}', [WelcomeController::class, 'index']);
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/users/{id}', [UserController::class, 'show']);
 
 Route::get('/', function () {
     return view('welcome');
