80149bcfb930b92dca0a2b8a689fe167c15d47e0
Diego R <drivarola@interclick.com.ar>
2021-03-30 13:32:50 -0300

    Add Laravel 5.1 files

---
+++ app/Http/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/welcome/{name}', [WelcomeController::class, 'index']);
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/users/{id}', [UserController::class, 'show']);
