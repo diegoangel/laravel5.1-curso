a9b6f10aa0f83687587a63f6d82c825a70ddc8b5
Diego R <drivarola@interclick.com.ar>
2021-03-31 15:57:37 -0300

    Add user index listing

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,3 +1,9 @@
 <h1>Listado de Usuarios!!!</h1>
 
 <!-- ACA IBA LA TABLA CON EL LISTADO DE USUARIOS PERO LA ELIMINE SIN QUERER -->
+
+@forelse ($users as $user)
+    <li>{{ $user->name }}</li>
+@empty
+    <p>No users</p>
+@endforelse
